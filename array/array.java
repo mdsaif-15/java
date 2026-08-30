71fb98e2da8e6a60ea609741c6a9a9439ce7a7fb
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-30 15:33:09 +0530

    array

---
+++ array/array.java
@@ -0,0 +1,13 @@
+public class array {
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+        arr[0] = 12;
+        arr[1] = 13;
+        arr[2] = 14;
+        arr[3] = 15;
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
