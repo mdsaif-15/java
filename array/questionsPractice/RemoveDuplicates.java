abb3e481d49a4b26a20fc9b726ba9521bb9d23df
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-09-05 17:22:53 +0530

    RemoveDuplicates

--- array/questionsPractice/RemoveDuplicates.java
+++ array/questionsPractice/RemoveDuplicates.java
@@ -2,15 +2,22 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
         int array[] = { 10, 20, 30, 10, 40, 50, 10, 40 };
         int newArray[] = new int[10];
+        int index = 0;
+        boolean found = false;
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (array[i] != array[j]) {
-                    newArray[i] = array[i];
+            for (int j = 0; j < index; j++) {
+                if (array[i] == newArray[j]) {
+                    found = false;
+                }
+                if (found) {
+                    newArray[index] = array[i];
+                    index++;
+                    found = true;
                 }
             }
         }
         for (int i = 0; i < newArray.length; i++) {
-            System.out.println(newArray[i] + " ");
+            System.out.println(newArray[i]);
         }
     }
 
